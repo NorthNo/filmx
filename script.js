02496ada3c979665d7c104418f6aae60b81ece37
NorthNo <108903009+NorthNo@users.noreply.github.com>
2025-05-04 21:49:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieList = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    let clickCounter = 0;
+    const ImageItem = movieList[i].querySelectorAll("img").length;
+
+    arrow.addEventListener("click", function () {
+        clickCounter++;
+        if (ImageItem - (6 + clickCounter) >= 0) {
+            movieList[i].style.transform = `translateX(${movieList[i].
+                computedStyleMap().get("transform")[0].x.value - 300}px)`;
+        } else {
+            movieList[i].style.transform = `translateX(0)`;
+            clickCounter = 0;
+        }
+    });
+});
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll("body,.navbar,.navbar a,.toggle,.toggle-ball,.sidebar,.type-filter select");
+
+ball.addEventListener("click", function(){
+    items.forEach((item) => item.classList.toggle("active"));
+});
